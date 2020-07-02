f3c7bdc54e907dfb17fa131c73a5b37b23c896d5
Jose Dalino <jrdalino@gmail.com>
2020-07-02 18:55:21 +0800

    Initial

--- ecr.tf
+++ ecr.tf
@@ -1,5 +1,11 @@
 # ECR Repository
 resource "aws_ecr_repository" "ecr_repository" {
-  name                 = "${var.ecr_repo_name}"
-  image_tag_mutability = "MUTABLE"  
+  name                 = var.ecr_repo_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  # tags
 }
--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,43 @@
 # ECR Repository
-output "terraform_ecr_repo_arn" {
-  value = "${aws_ecr_repository.ecr_repository.arn}"
+output "aws_ecr_repository_arn" {
+  value = aws_ecr_repository.ecr_repository.arn
+}
+
+output "aws_ecr_repository_name" {
+  value = aws_ecr_repository.ecr_repository.name
+}
+
+output "aws_ecr_repository_registry_id" {
+  value = aws_ecr_repository.ecr_repository.registry_id
+}
+
+output "aws_ecr_repository_repository_url" {
+  value = aws_ecr_repository.ecr_repository.repository_url
 }
 
 # S3 Bucket for Artifacts
-output "s3_bucket_artifacts_arn" {
-  value = "${aws_s3_bucket.build_artifact_bucket.arn}"
+output "aws_s3_bucket_id" {
+  value = aws_s3_bucket.build_artifact_bucket.id
+}
+
+output "aws_s3_bucket_arn" {
+  value = aws_s3_bucket.build_artifact_bucket.arn
 }
 
 # CodeBuild
 output "codebuild_service_role_arn" {
-  value = "${aws_iam_role.codebuild_service_role.arn}"
+  value = aws_iam_role.codebuild_service_role.arn
 }
 
 output "codebuild_project_arn" {
-  value = "${aws_codebuild_project.build_project.arn}"
+  value = aws_codebuild_project.build_project.arn
 }
 
 # CodePipeline
 output "codepipeline_service_role_arn" {
-  value = "${aws_iam_role.codepipeline_service_role.arn}"
+  value = aws_iam_role.codepipeline_service_role.arn
 }
 
 output "codepipeline_pipeline_arn" {
-  value = "${aws_codepipeline.codepipeline.arn}"
+  value = aws_codepipeline.codepipeline.arn
 }
